c65da2a8ef62688ff174ebf532f2bd6006bed688
Thales Brasileiro <thalesxbrasileiro@gmail.com>
2024-04-10 14:35:32 -0300

    fix: Erro ao tentar buscar todos os cupons

--- src/main/java/br/com/dbc/vemser/iShirts/dto/cupom/CupomCreateDTO.java
+++ src/main/java/br/com/dbc/vemser/iShirts/dto/cupom/CupomCreateDTO.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 import javax.validation.constraints.Future;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.time.LocalDateTime;
 
 @Data
